013ab846dcadb611245bdb911a81d8332a6b70e8
Lorel <lorel@wellatribe.com>
2016-11-11 16:10:19 +0100

    docker-compose.yml compliant with docker-compose v2

--- experiment/print-results.lua
+++ experiment/print-results.lua
@@ -2,8 +2,6 @@ local ZmqRx = require 'zmq-rx'
 
 local from_socket = os.getenv('FROM') or 'tcp://*:5558'
 
-ZmqRx.openController()
-
 ZmqRx.Observable.fromZmqSocket(from_socket) -- 'tcp://*:5558'
   :subscribe(
     function(results)
--- experiment/test/run.sh
+++ experiment/test/run.sh
@@ -6,5 +6,4 @@ cp -f ../filter-event.lua filter-event.lua
 cp -f ../reduce-events.lua reduce-events.lua
 cp -f ../print-results.lua print-results.lua
 
-docker-compose run data
-docker logs test_print_1
+docker-compose up printer
