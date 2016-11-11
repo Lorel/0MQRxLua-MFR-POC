c65257cf549503649f58354d93d01cca874d368a
Lorel <lorel@wellatribe.com>
2016-11-11 17:06:15 +0100

    hack to make it work

--- experiment/test/run.sh
+++ experiment/test/run.sh
@@ -6,4 +6,8 @@ cp -f ../filter-event.lua filter-event.lua
 cp -f ../reduce-events.lua reduce-events.lua
 cp -f ../print-results.lua print-results.lua
 
+docker-compose up -d data
+docker-compose up -d mapper
+docker-compose up -d filter
+docker-compose up -d reduce
 docker-compose up printer
