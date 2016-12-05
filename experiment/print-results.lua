56b8b37c2e7975df9ccfa089163d593c86e67dd0
Lorel <lorel@wellatribe.com>
2016-12-05 16:24:04 +0100

    reduce reducers

--- experiment/print-results.lua
+++ experiment/print-results.lua
@@ -2,15 +2,17 @@ local ZmqRx = require 'zmq-rx'
 
 local from_socket = os.getenv('FROM') or 'tcp://localhost:5558'
 
-file = io.open("output/" .. (os.getenv('OUTPUT') or 'results.dat'), 'a')
+local file = io.open("output/" .. (os.getenv('OUTPUT') or 'results.dat'), 'a')
 io.output(file)
-start = os.time()
+local start = os.time()
+local results = {}
 
 ZmqRx.Observable.fromZmqSocket(from_socket) -- 'tcp://localhost:5558'
   :subscribe(
-    function(results)
-      for k,v in pairs(results) do
-        print('Delays for carrier ' .. k .. ' -> ' .. (v.count == 0 and 0 or (v.total / v.count)) .. ' average mins - ' .. math.tointeger(v.count) .. " delayed flights")
+    function(datas)
+      for k,v in pairs(datas) do
+        carrier = results[k] or {}
+        results[k] = { count = (carrier.count or 0) + v.count, total = (carrier.total or 0) + v.total }
       end
     end,
     function(error)
@@ -19,6 +21,9 @@ ZmqRx.Observable.fromZmqSocket(from_socket) -- 'tcp://localhost:5558'
     function()
       io.write((os.time() - start) .. "\n")
       io.close(file)
+      for k,v in pairs(results) do
+        print('Delays for carrier ' .. k .. ' -> ' .. (v.count == 0 and 0 or (v.total / v.count)) .. ' average mins - ' .. math.tointeger(v.count) .. " delayed flights")
+      end
       ZmqRx.sendZmqCompleted()
       print('completed!')
     end
