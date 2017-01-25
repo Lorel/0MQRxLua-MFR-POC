2738831cd24e08c0bd80467636cfc2c7758839ce
Valerio Schiavoni <valerio.schiavoni@gmail.com>
2017-01-25 18:36:45 +0100

    template for DEBS17

---
+++ papers/DEBS17/make.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+for i in `find . -name *.eps`
+do
+    f_=${i%.eps}
+    if [[ $f_.eps -nt $f_.pdf || ! -e $f_.pdf ]]
+    then
+	epstopdf $i
+    fi
+done
+
+pdflatex paper
+bibtex   paper
+pdflatex paper
+pdflatex paper
+
