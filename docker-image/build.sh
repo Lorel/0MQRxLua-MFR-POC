737a8ebbccb7a89b03da787ee6581cf269094bff
Lorel <lorel@wellatribe.com>
2017-02-25 21:04:48 +0100

    upgrade docker image build

--- docker-image/build.sh
+++ docker-image/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-IMAGE=lorel/zmqrxlua-poc:back-to-push-pull
+IMAGE=lorel/zmqrxlua-poc:lua-5.3
 
 cp -f ../experiment/zmq-rx.lua build_files/zmq-rx.lua
 docker build -t $IMAGE .
