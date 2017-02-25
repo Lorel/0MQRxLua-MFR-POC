2315c3174c690c487a70b930c66a4b8cef59869a
Lorel <lorel@wellatribe.com>
2017-02-25 21:02:50 +0100

    add guard to prevent SegmentationFault crash from luacsv on nil value in mapper

--- experiment/map-csv-to-event.lua
+++ experiment/map-csv-to-event.lua
@@ -8,6 +8,7 @@ local to_socket = os.getenv('TO') or 'tcp://localhost:5556'
 ZmqRx.Subject.fromZmqSocket(from_socket) -- 'tcp://localhost:5555'
   :map(
     function(value)
+      if not value then return {} end
       -- print('process value', value)
       local array = csv.parse(value)
       local event = {}
