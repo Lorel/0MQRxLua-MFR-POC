21d337fc498c2e7405a8c2cc72af2e14dae78143
Lorel <lorel@wellatribe.com>
2017-03-03 21:21:02 +0100

    implement filterSGX and reduceSGX (and awesome tests)

---
+++ experiment/sgx-filter-event.lua
@@ -0,0 +1,13 @@
+local ZmqRx = require 'zmq-rx'
+
+local from_socket = os.getenv('FROM') or 'tcp://localhost:5556'
+local to_socket = os.getenv('TO') or 'tcp://localhost:5557'
+
+ZmqRx.Subject.fromZmqSocket(from_socket) -- 'tcp://localhost:5556'
+  :filter(
+    function(event)
+      local delay = tonumber(event.arrdelay) or 0
+      return delay > 0
+    end
+  )
+  :subscribeToSocket(to_socket) -- 'tcp://localhost:5557'
